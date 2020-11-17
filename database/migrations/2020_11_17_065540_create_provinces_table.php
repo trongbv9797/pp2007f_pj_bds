38b5dc7e86f31ce70a40ed0fd81d4f8ce4193dad
trongbv <trongbv9797@gmail.com>
2020-11-17 15:16:03 +0700

    create_migration_and_seed_provinces_table

---
+++ database/migrations/2020_11_17_065540_create_provinces_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvincesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('type');
+            $table->string('name_with_type');
+            $table->integer('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provinces');
+    }
+}

97cf1fb3de6e4ac9c61170ee8f566fd0e03d7007
trongbv <trongbv9797@gmail.com>
2020-11-17 15:45:01 +0700

    conflict_create_migration_and_seed_districts_table

---
+++ database/seeds/DistrictSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\model\District;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DistrictSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('districts')->delete();
+        $string = file_get_contents(__DIR__.'/dist/quan_huyen.json');
+        $json_a = json_decode($string, true);
+        foreach($json_a as $item){
+            DB::table('districts')->insert([
+                'name' => $item['name'],
+                'type' => $item['type'],
+                'slug' => $item['slug'],
+                'name_with_type' => $item['name_with_type'],
+                'path' => $item['path'],
+                'path_with_type' => $item['path_with_type'],
+                'code' => $item['code'],
+                'parent_code' => $item['parent_code'],
+                'created_at'=> date('y-m-d h:i:s'),
+                'updated_at'=> date('y-m-d h:i:s'),
+            ]);
+        }
+        
+    }
+}

8d7691189304a5999cd0dd256f832b42fb9501f5
kviet91 <kviet91.cdp@gmail.com>
2020-11-18 13:55:33 +0700

    repair_coposer.lock

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(DistrictSeeder::class);
 
         $this->call(ProvinceSeeder::class);
-        $this->call(CategorySeeder::class);
+
         $this->call(ProductSeeder::class);
 
 

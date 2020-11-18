2ee4e181d66e61070da09ca44209ed60fab76259
TaiAnh123 <nguyenluongtaianh@gmail.com>
2020-11-18 10:57:20 +0700

    changes

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -20,12 +21,13 @@ class ProductSeeder extends Seeder
         'price' => rand(1000000, 1000000000),
         'area' => rand(20, 200),
         'image' => ('http://lorempixel.com/640/480/'),
+        'slug' => rand(1, 100000),
         'number_of_bedroom' => rand(1, 5),
         'number_of_restroom' => rand(1, 3),
         'number_of_floor' => rand(1, 5),
         'address' => $faker->address,
         'content' => $faker->paragraph($nbSentences = 40, $variableNbSentences = true),
-        'city_id' => rand(1, 64),
+        'provinces_id' => rand(1, 64),
         'district_id' => rand(1, 100),
         'street_id' => rand(1, 200),
         'created_at' => date('Y-m-d H:i:s'),

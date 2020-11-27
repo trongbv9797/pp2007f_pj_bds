f14adfb497a77fee344ab80188c5183009011f60
kviet91 <kviet91.cdp@gmail.com>
2020-11-27 13:38:43 +0700

    create_admin_user_page_

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('fullname');
+            $table->boolean('sex');
+            $table->date('dateofbirth');
+            $table->string('phonenumber');
+            $table->string('avatar');
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });

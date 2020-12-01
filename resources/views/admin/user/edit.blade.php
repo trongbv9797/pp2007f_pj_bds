a6fbe74a24905e0cd50e51188adcd77be7914e0a
kviet91 <kviet91.cdp@gmail.com>
2020-12-01 16:08:32 +0700

    update avatar user

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -14,11 +14,7 @@
         <div class="header-sub-title">
             <nav class="breadcrumb breadcrumb-dash">
                 <a href="#" class="breadcrumb-item"><i class="ti-home p-r-5"></i>Admin</a>
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                <a class="breadcrumb-item" href="#">Create</a>
-=======
                 <a class="breadcrumb-item" href="#">User</a>
->>>>>>> delete_user_not_ajax
                 <span class="breadcrumb-item active">Edit User</span>
             </nav>
         </div>
@@ -31,30 +27,18 @@
             <div class="row">
                 <div class="col-sm-8">
                     
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                    <form role="form" id="form-validation" method="post" action="{!!  Route('storeUser') !!}">
-=======
-                    <form role="form" id="form-validation" method="post" action="{{ route('updateUser',$user->id) }}">
->>>>>>> delete_user_not_ajax
+                    <form role="form" id="form-validation" method="post" action="{{ route('updateUser',$user->id) }}" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label control-label">User Name *</label>
                             <div class="col-sm-10">
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                                <input type="text" class="form-control" name="username" placeholder="Enter your user name"required>
-=======
                                 <input type="text" class="form-control" name="username" placeholder="{{ $user->username }}"required>
->>>>>>> delete_user_not_ajax
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label control-label" >Email *</label>
                             <div class="col-sm-10">
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                                <input type="email" class="form-control" name="email" placeholder="Enter your email"required>
-=======
                                 <input type="email" class="form-control" name="email" placeholder="{{ $user->email }}"required>
->>>>>>> delete_user_not_ajax
                             </div>
                         </div>
                         <div class="form-group row">
@@ -74,11 +58,7 @@
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label control-label">Full Name</label>
                             <div class="col-sm-10">
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                                <input type="text" class="form-control" name="fullname" placeholder="Enter your full name"
-=======
                                 <input type="text" class="form-control" name="fullname" placeholder="{{ $user->fullname }}"
->>>>>>> delete_user_not_ajax
                                     required>
                             </div>
                         </div>
@@ -89,12 +69,15 @@
                                 <div class="icon-input">
                                     <i class="mdi mdi-timer"></i>
                                     <input id="datepicker-1" data-provide="datepicker" type="text" class="form-control" name="dateofbirth"
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                                        placeholder="Pick your date of birth">
-=======
                                         placeholder="{{ $user->dateofbirth }}">
->>>>>>> delete_user_not_ajax
                                 </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label control-label">Phone Number</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="phonenumber"
+                                    placeholder="Enter your phone number">
                             </div>
                         </div>
 
@@ -102,13 +85,17 @@
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label control-label">Address</label>
                             <div class="col-sm-10">
-<<<<<<< bc66557b26f139d594431141c8e8fe256da0c913
-                                <input type="text" class="form-control" name="address" placeholder="Enter your address">
-=======
                                 <input type="text" class="form-control" name="address" placeholder="{{ $user->address }}">
->>>>>>> delete_user_not_ajax
                             </div>
                         </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label control-label">Avatar</label>
+                            <div class="col-sm-10">
+                                <input type="file" class="form-control" name="avatar1">
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label control-label">Gender</label>
                             <div class="col-sm-10">
@@ -123,7 +110,7 @@
                             </div>
                         </div>
 
-                        <input type="submit" class="btn btn-gradient-success" value="Submit">
+                        <input type="submit" class="btn btn-gradient-success" value="Edit">
                     </form>
                 </div>
             </div>
